ee006641bf14dc22c706c5f3595c36fb338e8456
Chocoeater <lonelypain98@gmail.com>
2024-12-22 00:05:40 +0700

    Исправление функции sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -9,17 +9,17 @@ def filter_by_state(list_of_data: list, state: str = 'EXECUTED') -> list:
     return result_list
 
 
-def sort_by_date(list_of_data: list, reverse: bool = True) -> list:
+def sort_by_date(list_of_data: list, decreasing: bool = True) -> list:
     """принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание). Функция возвращает новый список, отсортированный по дате.
     Использует пузырьковый алгоритм сортировки"""
     result = list_of_data
-    if reverse:
+    if decreasing:
         for i in range(len(result) - 1):
-            for j in range(len(result) - i):
+            for j in range(len(result) - 1 - i):
                 if result[j]['date'][:10] < result[j + 1]['date'][:10]:
-                    result[j]['date'][:10], result[j + 1]['date'][:10] = \
-                        result[j + 1]['date'][:10], result[j]['date'][:10]
+                    result[j]['date'], result[j + 1]['date'] = \
+                        result[j + 1]['date'], result[j]['date']
     else:
         for i in range(len(result) - 1):
             for j in range(len(result) - 1 - i):
