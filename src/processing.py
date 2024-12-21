f9a087142e9c2bd27de74fceb08a18bed964c9e8
Chocoeater <lonelypain98@gmail.com>
2024-12-21 23:59:17 +0700

    Добавил README файл и сделал мелкий фикс функции sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -9,7 +9,7 @@ def filter_by_state(list_of_data: list, state: str = 'EXECUTED') -> list:
     return result_list
 
 
-def sort_by_date(list_of_data: list, reverse: bool = False) -> list:
+def sort_by_date(list_of_data: list, reverse: bool = True) -> list:
     """принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание). Функция возвращает новый список, отсортированный по дате.
     Использует пузырьковый алгоритм сортировки"""
@@ -17,13 +17,13 @@ def sort_by_date(list_of_data: list, reverse: bool = False) -> list:
     if reverse:
         for i in range(len(result) - 1):
             for j in range(len(result) - i):
-                if result[j]['date'][:10] > result[j + 1]['date'][:10]:
+                if result[j]['date'][:10] < result[j + 1]['date'][:10]:
                     result[j]['date'][:10], result[j + 1]['date'][:10] = \
                         result[j + 1]['date'][:10], result[j]['date'][:10]
     else:
         for i in range(len(result) - 1):
             for j in range(len(result) - 1 - i):
-                if result[j]['date'][:10] < result[j + 1]['date'][:10]:
+                if result[j]['date'][:10] > result[j + 1]['date'][:10]:
                     result[j]['date'], result[j + 1]['date'] = \
                         result[j + 1]['date'], result[j]['date']
     return result
