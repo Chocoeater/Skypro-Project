e7151d35e16e69c3d611021dfce85eb8f55ac5ef
Chocoeater <lonelypain98@gmail.com>
2025-01-18 21:54:26 +0700

    add tests for decorators and htmlcov

--- src/decorators.py
+++ src/decorators.py
@@ -1,4 +1,5 @@
-from typing import TypeVar, ParamSpec, Optional, Callable
+from functools import wraps
+from typing import Callable, Optional, ParamSpec, TypeVar
 
 P = ParamSpec('P')  # Для аргументов
 R = TypeVar('R')  # Для возвращаемых значений
@@ -8,13 +9,14 @@ def log(filename: Optional[str] = None) -> Callable[[Callable[P, R]], Callable[P
     """Записывает в файл или в консоль статус выполнения функции (успешно или ошибка)"""
 
     def my_dec(function: Callable[P, R]) -> Callable[P, None]:
+        @wraps(function)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> None:
             try:
                 function(*args, **kwargs)
             except Exception as e:
-                message = f'{function} error: {e}. Inputs: {args, kwargs}\n'
+                message = f'{function.__name__} error: {e}. Inputs: {args, kwargs}'
             else:
-                message = f'{function} ok\n'
+                message = f'{function.__name__} ok'
             finally:
                 if filename:
                     with open(filename, 'w', encoding='utf-8') as file:
